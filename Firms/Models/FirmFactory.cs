06565935c06d3398247aa01ed6a4885834df3693
Konstantin Ignakov <kmignakov@mts.ru>
2023-09-23 17:45:55 +0300

    create firm factory as singleton

---
+++ Firms/Models/FirmFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Firms.Models;
+
+public class FirmFactory
+{
+	private FirmFactory() { }
+	private FirmFactory? _factory = null;
+
+	public FirmFactory? Factory
+	{
+		get
+		{
+			this._factory ??= new FirmFactory();
+			return this._factory;
+		}
+	}
+}
