7afdbc687fe8fc03cd6468cd7d88c20dfdf4cd34
Konstantin Ignakov <kmignakov@mts.ru>
2023-09-23 20:28:00 +0300

    create ContactType model

--- Firms.Domain/Models/Contact.cs
+++ Firms.Domain/Models/Contact.cs
@@ -4,16 +4,17 @@ public class Contact
 {
 	private Contact() { }
 
-	public Contact(DateTime? endDt, string description, string dataInfo)
+	public Contact(DateTime? endDt, string description, string dataInfo, ContactType contactType)
 	{
 		EndDt = endDt;
 		Description = description;
 		DataInfo = dataInfo;
+		ContactType = contactType;
 	}
 
 	public DateTime BeginDt = DateTime.Now;//Дата начала контакта
 	public DateTime? EndDt { get; private set; } = null;//Дата завершения контакта
 	public string Description { get; private set; } = null!;//Описание контакта для себя
 	public string DataInfo { get; private set; } = null!;//Формулировка контакта для клиента
-	//public ContType ContactType;//Вид контакта
+	public ContactType ContactType { get; private set; } //Вид контакта
 }
---
+++ Firms.Domain/Models/ContactType.cs
@@ -0,0 +1,9 @@
+﻿namespace Firms.Domain.Models;
+
+public class ContactType
+{
+	private ContactType() { }
+
+	public string Name { get; private set; } = null!;
+	public string Note { get; private set; } = null!;
+}
