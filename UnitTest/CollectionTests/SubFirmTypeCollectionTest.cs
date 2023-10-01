f14cde962f805d88889ae1e35282f50574e03a3b
Konstantin Ignakov <ignakov37@gmail.com>
2023-10-01 13:30:14 +0300

    update SubFirmTypeCollection test

--- UnitTest/CollectionTests/SubFirmTypeCollectionTest.cs
+++ UnitTest/CollectionTests/SubFirmTypeCollectionTest.cs
@@ -8,6 +8,8 @@ public class SubFirmTypeCollectionTest
 	[TestMethod]
 	public void TestForEach()
 	{
+		#region arrange
+
 		var collection = new SubFirmTypeCollection(new List<SubFirmType>
 		{
 			new(true, "name 1"),
@@ -21,6 +23,39 @@ public class SubFirmTypeCollectionTest
 			Console.WriteLine($"{item.Name}: {item.IsMain}");
 		}
 
+		#endregion arrange
+
+		#region assert
+
 		Assert.IsTrue(true);
+
+		#endregion assert
+	}
+
+	[TestMethod]
+	public void ValidateCount()
+	{
+		#region arrange
+
+		var mainCollection = new List<SubFirmType>
+		{
+			new(true, "name 1"),
+			new(false, "name 2"),
+			new(false, "name 3"),
+			new(false, "name 4"),
+		};
+
+		var collection = new SubFirmTypeCollection(mainCollection);
+
+		var localCollectionCount = mainCollection.Count;
+		var collectionCount = collection.Count;
+
+		#endregion arrange
+
+		#region assert
+
+		Assert.AreEqual(localCollectionCount, collectionCount);
+
+		#endregion assert
 	}
 }
