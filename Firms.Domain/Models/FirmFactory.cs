3945c4e07dd206f0c81ac701bdc11a139db6ae60
Konstantin Ignakov <kmignakov@mts.ru>
2023-10-15 21:41:02 +0300

    update firmfactory and add test for it

--- Firms.Domain/Models/FirmFactory.cs
+++ Firms.Domain/Models/FirmFactory.cs
@@ -4,13 +4,23 @@ public class FirmFactory
 {
 	private FirmFactory() { }
 	private static FirmFactory? _factory = null;
+	public static List<Firm> Firms { get; private set; } = new();
 
-	public static FirmFactory? Factory
+	public static FirmFactory Factory
 	{
 		get
 		{
 			_factory ??= new FirmFactory();
 			return _factory;
 		}
+	}
+
+	public Firm Create(string name, string shortName, string country, string region, string town, string street,
+		string postIndex, string email, string web)
+	{
+		var firm = new Firm(name, shortName, country, region, town, street,
+			postIndex, email, web);
+		Firms.Add(firm);
+		return firm;
 	}
 }
---
+++ UnitTest/ModelTests/FirmFactoryTest.cs
@@ -0,0 +1,39 @@
+﻿using Firms.Domain.Models;
+
+namespace UnitTest.ModelTests;
+
+[TestClass]
+public class FirmFactoryTest
+{
+	[TestMethod]
+	public void TestCreateFirmFromFabric()
+	{
+		var localFirm = new Firm("Name", "ShortName", "Country", "Region", "Town", "Street", "postIndex", "Email",
+			"Web");
+
+		var fabricFirm = FirmFactory
+			.Factory.Create(
+				localFirm.Name,
+				localFirm.ShortName,
+				localFirm.Country,
+				localFirm.Region,
+				localFirm.Town,
+				localFirm.Street,
+				localFirm.PostIndex,
+				localFirm.Email,
+				localFirm.Web);
+
+		Assert.IsNotNull(fabricFirm);
+		Assert.IsTrue(FirmFactory.Firms.Count > 0);
+		Assert.AreEqual(localFirm.Name, fabricFirm.Name);
+		Assert.AreEqual(localFirm.ShortName, fabricFirm.ShortName);
+		Assert.AreEqual(localFirm.Country, fabricFirm.Country);
+		Assert.AreEqual(localFirm.Region, fabricFirm.Region);
+		Assert.AreEqual(localFirm.Town, fabricFirm.Town);
+		Assert.AreEqual(localFirm.Street, fabricFirm.Street);
+		Assert.AreEqual(localFirm.PostIndex, fabricFirm.PostIndex);
+		Assert.AreEqual(localFirm.Email, fabricFirm.Email);
+		Assert.AreEqual(localFirm.Web, fabricFirm.Web);
+	}
+
+}
