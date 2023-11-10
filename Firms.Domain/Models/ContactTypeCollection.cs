c5fff86ba06350b61dde48fa317a7d6ec7537616
Konstantin Ignakov <ignakov37@gmail.com>
2023-11-10 21:27:13 +0300

    123

---
+++ Firms.Domain/Models/ContactTypeCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Firms.Domain.Models;
+
+public class ContactTypeCollection : IEnumerable<ContactType>
+{
+	private readonly List<ContactType> _lst = new();
+
+	public ContactTypeCollection(List<ContactType> value)
+	{
+		_lst = value;
+	}
+
+	public int Count => _lst.Count;
+
+	public void Add(ContactType type)
+		=> _lst.Add(type);
+
+	public void Clear() => _lst.Clear();
+
+	public IEnumerator<ContactType> GetEnumerator()
+		=> _lst.GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
